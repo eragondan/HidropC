edd562196cbfff93879fd46fee001e44dc0a80e8
Dano <eragond@hotmail.com>
2018-04-26 18:08:35 -0500

    some

--- Centro.c
+++ Centro.c
@@ -229,7 +229,7 @@ void imprimeValoresConfiguracion(){
      }
 void modificaConfigBin(){
       FILE *archivoConfig;
-      archivoConfig=fopen("config.cfg", "wb");
+      archivoConfig=fopen("config.cfg", "wb+");
       fseek(archivoConfig,0*sizeof(configuracion),SEEK_SET);
       fwrite(&configuracionActual,sizeof(configuracion),1,archivoConfig);
       fclose(archivoConfig);
@@ -286,6 +286,7 @@ void modificaConfig(){
                   imprimeConsolinI(configuracionActual.confRiegoDur, "DUR");
              break;
              case 16: //Regresar
+                   printImagenFondo(2);
                    return 'x';
              break;
          }
@@ -345,7 +346,114 @@ int printImagenFondo(int numeroFondo){
       fclose(archivoImagen);
       return 0;
 }
-
+//----------------------------------------FUNCIONES DE INTERFAZ PRINCIPAL--------------------------------------------------------
+void printDatos(){
+   gotoxy(14,3);
+     printf(dataActual.nomCultivo); //Nombre del Cultivo
+   gotoxy(39,2);
+     printf(dataActual.fecha); // Fecha 
+   gotoxy(50,2);
+     printf("30:70");
+   gotoxy(32,5);
+     printf("%.2f",dataActual.temperatura); //Temperatura 
+   gotoxy(41,5);
+     printf("%.2f",configuracionActual.confMaxTemperatura); //Maximo de la Temperatura
+   gotoxy(50,5);
+     printf("%.2f",configuracionActual.confMinTemperatura); //Minimo de la Temperatura
+   gotoxy(32,6);
+     printf("%.2f",dataActual.humedad); //Nivel de Humedad
+   gotoxy(41,6);
+     printf("%.2f",configuracionActual.confMaxHumedad); //Maximo de Humedad
+   gotoxy(50,6);
+     printf("%.2f",configuracionActual.confMinHumedad); //Minimo de Humedad
+   gotoxy(32,7);
+     printf("%.2f",dataActual.senterm); // Sensación Termica
+   gotoxy(32,11);
+     if(dataActual.leds!=0)  //Luces Led
+         printf("ON ");
+   gotoxy(32,12);
+     if(dataActual.bomba!=0) //Bomba
+         printf("ON ");
+   gotoxy(32,13);
+     if(dataActual.ventanas!=0) //Ventana
+         printf("ON ");
+   gotoxy(32,14);
+     if(dataActual.foco!=0) //Foco
+         printf("ON ");
+   gotoxy(41,11); // Tiempo Inicio de Iluminacion
+     printf("20:30");
+   gotoxy(41,12); // Tiempo Inicio de Riego
+     printf("30:20");
+   gotoxy(51,11);
+     printf("%i",configuracionActual.confIlumiDur); // Duración de la Ilumninacion
+   gotoxy(51,12);
+    printf("%i",configuracionActual.confRiegoDur); //Duracióo del Riego
+        
+   return;
+}
+void modificaPrincipal(){
+    char opciones(int *x){
+         switch(*x){
+             case 11: // Iluminacion
+                
+             break;
+             case 12: //Riego
+                
+             break;
+             case 13: //Ventilación
+                
+             break;
+             case 14: //Calentador
+                 
+             break;
+             case 16: // Guardar
+                  
+             break;
+             case 17: //Revisar
+                 
+             break;
+             case 18: //Editar
+                  printImagenFondo(4);
+                  modificaConfig();
+             break;
+             case 19: //Salir
+                 printImagenFondo(1);
+                 getch();
+                 return 'x';
+             break;
+         }
+        return;
+    }
+    char opcion;
+    int x=6, y=11;
+    while(opcion!='x'){
+       printDatos();
+       opcion=getch();
+       gotoxy(x, y);
+       putchar('\0');
+       switch (opcion){
+       
+           case 'w'://arriba
+                if(y>11){
+                        y-=1;
+                        if(y==15)
+                         y=14;}
+           break;
+           case 's'://abajo
+                if(y<=18){
+                        y+=1; 
+                        if(y==15)
+                         y=16;}
+           break;  
+           case 'd'://introducir
+               opcion=opciones(&y);
+           break;   
+       }
+       gotoxy(x, y);
+       putchar(62);
+    };
+    return;
+}
 //El buffer maximo es de 60 en X y 20 en Y
 //El buffer minimo es de 1 en X y 1 en Y*/
 //--------------------------------------------------MAIN---------------------------------------------------
@@ -355,14 +463,29 @@ int main(){
     printImagenFondo(1);
     getch();
     printImagenFondo(2);
-    getch();
-    printImagenFondo(4);
-    modificaConfig();
-    /*while(1){
-    getData();
-    Sleep(1000);
-    } */
-    getch();
+    strcpy(dataActual.nomCultivo,"AJO"); 
+    strcpy(dataActual.fecha,"20/20/00");
+    dataActual.temperatura=12.21;
+    configuracionActual.confMaxTemperatura=23.32;
+    configuracionActual.confMinTemperatura=32.23;
+    dataActual.humedad=21.12;
+    configuracionActual.confMaxHumedad=34.43;
+    configuracionActual.confMinHumedad=43.34;
+    dataActual.senterm=78.89;
+    dataActual.leds=0;
+    dataActual.bomba=0;
+    dataActual.ventanas=0;
+    dataActual.foco=0;
+    configuracionActual.confIlumiDur=2;
+    configuracionActual.confRiegoDur=3;
+    modificaPrincipal();
+    
+
+    /*   prepVentana();
+   printImagenFondo(2);
+   printDatos();
+   modificaPrincipal();
+   getch();*/
     return 0;
 }
 //Ejemplos de uso de funciones
@@ -376,3 +499,8 @@ int main(){
     printf("%s",dataActual.fecha);
     tiempoCompu();
     printf("%i",dataActual.tiempo);*/
+    //Pedir datos
+    /*while(1){
+    getData();
+    Sleep(1000);
+    } */
